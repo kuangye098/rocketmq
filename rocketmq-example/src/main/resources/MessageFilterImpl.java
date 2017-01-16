5fd77d0319e0b130d2107667a22d7a6a03614eaf
kuangye <732184166@qq.com@490ae793-36cd-487e-ba82-1a6a4bbb6b5e>
2017-01-16 16:37:55 +0000

    移除不需要的东西，并修正从3.2.6版本遗留下来的与3.5.8版本不兼容的代码

--- rocketmq-example/src/main/resources/MessageFilterImpl.java
+++ rocketmq-example/src/main/resources/MessageFilterImpl.java
@@ -1,5 +1,6 @@
 package com.alibaba.rocketmq.example.filter;
 
+import com.alibaba.rocketmq.common.filter.FilterContext;
 import com.alibaba.rocketmq.common.filter.MessageFilter;
 import com.alibaba.rocketmq.common.message.MessageExt;
 
@@ -7,12 +8,11 @@ import com.alibaba.rocketmq.common.message.MessageExt;
 public class MessageFilterImpl implements MessageFilter {
 
     @Override
-    public boolean match(MessageExt msg) {
-        String property = msg.getProperty("SequenceId");
+    public boolean match(MessageExt msg, FilterContext context) {
+        String property = msg.getUserProperty("SequenceId");
         if (property != null) {
             int id = Integer.parseInt(property);
-            if (((id % 10) == 0) && //
-                    (id > 100)) {
+            if ((id % 3) == 0 && (id > 10)) {
                 return true;
             }
         }
