06b009a6ffd28f240f21da5e8fa39ddf35bb6b86
kuangye <732184166@qq.com>
2017-01-16 03:23:08 +0000

    移除3.5.8不支持的命令

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/filter/MessageFilterImpl.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/filter/MessageFilterImpl.java
@@ -1,14 +1,15 @@
 package com.alibaba.rocketmq.example.filter;
 
+import com.alibaba.rocketmq.common.filter.FilterContext;
 import com.alibaba.rocketmq.common.filter.MessageFilter;
 import com.alibaba.rocketmq.common.message.MessageExt;
 
 
 public class MessageFilterImpl implements MessageFilter {
 
-    @Override
-    public boolean match(MessageExt msg) {
-        String property = msg.getUserProperty("SequenceId");
+	@Override
+	public boolean match(MessageExt msg, FilterContext context) {
+		String property = msg.getUserProperty("SequenceId");
         if (property != null) {
             int id = Integer.parseInt(property);
             if ((id % 3) == 0 && (id > 10)) {
@@ -17,5 +18,5 @@ public class MessageFilterImpl implements MessageFilter {
         }
 
         return false;
-    }
+	}
 }
